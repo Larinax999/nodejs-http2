1864f7d58cfb37d388bd132d98d6b975d1df5af7
Larina <49904492+Larinax999@users.noreply.github.com>
2021-09-10 02:51:42 +0700

    add response headers

--- http2.js
+++ http2.js
@@ -1,22 +1,34 @@
 const http2 = require('http2');
-const domain = "discord.com"
+const domain = "discord.com";
 const client = http2.connect(`https://${domain}`);
 let data = [];
-//const body = "hello=world";
-//const buffer = Buffer.from(JSON.stringify(body));
-const req = client.request({
-	":scheme": "https",
-	":method": "GET",
-	":path":"/",
-	"referrer": `https://${domain}/`,
-	"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36 Edg/92.0.902.84"
+
+const fetch = (method,path,payload=null) => new Promise((resolve) => {
+	const client = http2.connect(`https://${domain}`);
+	const req = client.request({
+		":authority": domain,
+		":scheme": "https",
+		":method": method,
+		":path":path,
+		"content-type": "application/x-www-form-urlencoded",
+		"referrer": `https://${domain}/`,
+		"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36 Edg/92.0.902.84"
+	});
+	let data = [];
+	let headers = null;
+	req.on('data', (chunk) => {
+		data.push(chunk);
+	});
+	req.on('end', () => {
+		return resolve({data:data.join(""),headers:headers});
+	});
+	req.on('response', (response)=> {
+		headers = JSON.parse(JSON.stringify(response));
+	});
+	req.setEncoding('utf8');
+	if (payload != null) {
+		req.write(Buffer.from(payload));
+	}
+	req.end();
 });
-req.on('data', (chunk) => {
-	data.push(chunk);
-});
-req.on('end', () => {
-	console.log(data.join(""));
-});
-req.setEncoding('utf8');
-//req.write(buffer);
-req.end();
+fetch("GET","/register");
